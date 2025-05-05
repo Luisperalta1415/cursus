cf8f485b690fb96014cdfa197be3925ec08ba2d5
Luis Peralta Munoz <lperalta@c1r6s3.42malaga.com>
2025-05-05 14:25:53 +0200

    putnbr

--- libft/libft.h
+++ libft/libft.h
@@ -40,6 +40,11 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
-char	*strdup(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
